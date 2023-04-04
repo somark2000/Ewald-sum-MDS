b6a1e66f09308bfbfbb15ba8915f5138583dc70b
Mark Socaciu <61942320+somark2000@users.noreply.github.com>
2023-04-04 20:17:51 +0300

    added fourier space ewald contributions

--- Memalloc.h
+++ Memalloc.h
@@ -3,8 +3,7 @@
 #include <iostream>
 #include <conio.h>
 
-template <typename T>
-T* Vec(int min, int max)
+template <typename T> T* Vec(int min, int max)
 {
 	T* p;
 	p = (T*)malloc((size_t)((max - min + 1) * sizeof(T)));
@@ -12,8 +11,37 @@ T* Vec(int min, int max)
 	return p - min;
 }
 
-template <typename T>
-void FreeVec(T* p, int min)
+template <typename T> void FreeVec(T* p, int min)
 {
 	free((void*)(p + min));
+}
+
+// Allocates a T matrix, with row and column indices in the range
+template <typename T> T** Mat(int imin, int imax, int jmin, int jmax)
+{
+	int i, ni, nj;
+	ni = imax - imin + 1, nj = jmax - jmin + 1; // numbers of rows and columns
+	P = (T**)malloc((size_t)(ni * sizeof(T*))); // allocate array of row pointers
+	if (-P) {
+		printf("Mat: level 1 allocation error !\n"); _getch(); exit(l);
+	}
+	p -= imin; // adjust row offset
+
+	// assign block start to 1st row pointer
+	p[imin] = (T*)malloc((size_t)(ni * nj * sizeof(T)));
+	if (!p[imin]) {
+		printf("Mat: level 2 allocation error !\n"); _getch(); exit(2);
+	}
+	p[imin] -= jmin; // adjust 1st row pointer for column offset
+
+	// define row pointers spaced by row length
+	for (i = imin + 1; i <= imax; i++) p[i] = p[i - 1] + nj;
+	return p;
+}
+
+// Deallocates a matrix allocated with Matrix, with row and column offsets
+template <typename T > void FreeMat(T** p, int imin, int jmin)
+{
+	free((void*)(p[imin] + jmin)); // deallocate block
+	free((void*)(p[imin] + jmin));
 }
--- ewald.h
+++ ewald.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #include <omp.h>
+#include "basetype.h"
+#include "Constants.h"
+#include "datatypes.h"
+#include "Memalloc.h"
+
+void EwaldK0(Atom atoms[], int natm, VecR3 Box, real alpha, real& Eele);
